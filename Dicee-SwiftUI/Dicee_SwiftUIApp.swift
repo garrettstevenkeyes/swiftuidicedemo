0a18e5b844d5181ea92805bdd124c6ecde433e87
Garrett Keyes <garrett.keyes@gmail.com>
2024-02-17 14:19:18 -0500

    Add space

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Garrett Keyes on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
